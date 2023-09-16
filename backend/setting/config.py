75055af437ff9b0450feb5be4136d7353fcae37f
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-09-16 18:06:10 +0800

    Add Day03 code

---
+++ backend/setting/config.py
@@ -0,0 +1,19 @@
+import os
+from functools import lru_cache
+
+from dotenv import load_dotenv
+
+class Settings():
+    app_name:str = "iThome2023 FastAPI Tutorial"
+    author:str = "Jason Liu"
+
+    app_mode: str = os.getenv("APP_MODE")
+    port:int = int(os.getenv("PORT"))
+    reload:bool = os.getenv("RELOAD")
+    database_url: str = os.getenv("DATABASE_URL")
+
+
+@lru_cache()
+def get_settings():
+    load_dotenv( f".env.{os.getenv('APP_MODE')}")
+    return Settings()
