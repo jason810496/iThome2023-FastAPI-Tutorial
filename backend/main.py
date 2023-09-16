2dfaa3a73a5fe640fbe66fb00effb263d051c0f9
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-09-16 23:00:32 +0800

    add Day04 Code

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,37 @@
 from fastapi import FastAPI
 
 from setting.config import get_settings
+from schemas.users import User as UserSchema
+from schemas.items import Item as ItemSchema
+
+fake_db = {
+    "users": {
+        1: {
+            "name": "John",
+            "age": 35,
+            "email": "john@fakemail.com",
+            "birthday": "2000-01-01",
+        },
+        2: {
+            "name": "Jane",
+            "age": 25,
+            "email": "jane@fakemail.com",
+            "birthday": "2010-12-04",
+        }
+    },
+    "items": {
+        1: {
+            "name": "iPhone 12",
+            "price": 1000,
+            "brand": "Apple"
+        },
+        2: {
+            "name": "Galaxy S21",
+            "price": 800,
+            "brand": "Samsung"
+        }
+    }
+}
 
 app = FastAPI()
 
@@ -22,8 +53,22 @@ def get_infor():
 
 @app.get("/users/{user_id}")
 def get_users(user_id: int, qry: str = None):
-    return {"user_id": user_id, "query": qry }
+    if user_id not in fake_db["users"]:
+        return {"error": "User not found"}
+    return {"user": fake_db['users'][user_id], "query": qry }
+
+@app.post("/users")
+def create_users(user: UserSchema):
+    fake_db["users"][user.id] = user
+    return user
 
 @app.get("/items/{item_id}")
 def get_items_without_typing(item_id, qry):
-    return {"item_id": item_id, "query": qry }
+    if item_id not in fake_db["items"]:
+        return {"error": "Item not found"}
+    return {"item": fake_db['items'][item_id], "query": qry }
+
+@app.post("/items")
+def create_items(item: ItemSchema):
+    fake_db["items"][item.id] = item
+    return item
