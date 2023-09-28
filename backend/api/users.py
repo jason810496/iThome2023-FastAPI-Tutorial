309d24b33fdd0ab36596e1be03829c5366ee19b9
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-09-28 18:07:44 +0800

    add Day13 codes

--- backend/api/users.py
+++ backend/api/users.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, HTTPException, status 
-from typing import List 
+from fastapi import APIRouter, HTTPException, status , Depends
+from typing import List , Annotated
 from pydantic import Field
 from sqlalchemy.orm import Session 
 from sqlalchemy import select , update , delete
@@ -8,19 +8,21 @@ from schemas import users as UserSchema
 from database.generic import get_db
 from models.user import User as UserModel 
 
-fake_db = get_db()
+from api.depends import check_user_id , pagination_parms, test_verify_token
+
 
 router = APIRouter(
     tags=["users"],
-    prefix="/api"
+    prefix="/api",
 )
+
+db_session:Session = get_db()
 
 @router.get("/users", 
         response_model=List[UserSchema.UserRead],
         response_description="Get list of user",  
 )
-def get_users(qry: str = None):
-    db_session:Session = get_db()
+def get_users(page_parms= Depends(pagination_parms)):
 
     stmt = select(UserModel.name,UserModel.id,UserModel.email,UserModel.avatar)
     users = db_session.execute(stmt).all()
@@ -29,7 +31,6 @@ def get_users(qry: str = None):
 
 @router.get("/users/{user_id}" , response_model=UserSchema.UserRead )
 def get_user_by_id(user_id: int, qry: str = None):
-    db_session:Session = get_db()
 
     stmt = select(UserModel.name,UserModel.id,UserModel.email,UserModel.avatar).where(UserModel.id == user_id)
     user = db_session.execute(stmt).first()
@@ -55,7 +56,6 @@ def create_user(newUser: UserSchema.UserCreate ):
     - **avatar** (optional)
 
     """
-    db_session:Session = get_db()
 
     stmt = select(UserModel.id).where(UserModel.email == newUser.email)
     user = db_session.execute(stmt).first()
@@ -79,15 +79,7 @@ def create_user(newUser: UserSchema.UserCreate ):
     return vars(user)
 
 @router.put("/users/{user_id}" , response_model=UserSchema.UserUpdateResponse )
-def update_user(user_id: int, newUser: UserSchema.UserUpdate ):
-    
-    db_session:Session = get_db()
-
-    stmt = select(UserModel.id).where(UserModel.id == user_id)
-    user = db_session.execute(stmt).first()
-
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+def update_user(newUser: UserSchema.UserUpdate,user_id:int=Depends(check_user_id) ):
     
     stmt = update(UserModel).where(UserModel.id == user_id).values(
         name=newUser.name,
@@ -103,14 +95,7 @@ def update_user(user_id: int, newUser: UserSchema.UserUpdate ):
     return newUser
 
 @router.put("/users/{user_id}/password", status_code=status.HTTP_204_NO_CONTENT)
-def update_user_password(user_id : int, newUser:UserSchema.UserUpdatePassword):
-    db_session:Session = get_db()
-
-    stmt = select(UserModel.id).where(UserModel.id == user_id)
-    user = db_session.execute(stmt).first()
-
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+def update_user_password(newUser:UserSchema.UserUpdatePassword,user_id:int=Depends(check_user_id)):
     
     stmt = update(UserModel).where(UserModel.id == user_id).values(
         password=newUser.password,
@@ -128,14 +113,7 @@ def create_user_deprecated(newUser: UserSchema.UserCreate ):
     return "deprecated"
 
 @router.delete("/users/{user_id}",status_code=status.HTTP_204_NO_CONTENT )
-def delete_users(user_id: int):
-    db_session:Session = get_db()
-
-    stmt = select(UserModel.id).where(UserModel.id == user_id)
-    user = db_session.execute(stmt).first()
-
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+def delete_users(user_id:int = Depends(check_user_id) ):
     
     stmt = delete(UserModel).where(UserModel.id == user_id)
     db_session.execute(stmt)
