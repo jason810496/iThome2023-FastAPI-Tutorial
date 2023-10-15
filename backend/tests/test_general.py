3439749c590f4008deb4b789a1f721fa7a1e3165
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-10-15 23:47:52 +0800

    add testing

--- backend/tests/test_general.py
+++ backend/tests/test_general.py
@@ -3,8 +3,9 @@ import pytest
 from functools import lru_cache
 import random
 
-USER_AMOUNT = 100
+USER_AMOUNT = 500
 QUERY_AMOUNT = 50000
+UPDATE_AMOUNT = 50
 
 @lru_cache()
 def get_user_data():
@@ -31,11 +32,21 @@ def get_random_user_list():
 
     return data
 
+def get_random_user_list_for_update():
+    user_list = get_user_data()
+    data = []
+    for i in range(UPDATE_AMOUNT):
+        data.append(random.choice(user_list))
+
+    return data
+
 def get_random_page_range_list():
     data = []
     for i in range(QUERY_AMOUNT):
-        l = random.randint(0,USER_AMOUNT-1)
-        r = random.randint(l+1,USER_AMOUNT)
+        l = random.randrange(0,USER_AMOUNT-2)
+        r = random.randrange(l+1,USER_AMOUNT-1)
+        # l = 0
+        # r = 50
         data.append( ( l, r ) )
 
     return data
@@ -77,11 +88,18 @@ async def test_create_user(async_client,user):
 @pytest.mark.asyncio
 async def test_get_users(async_client,l,r):
     limit = r - l
+    print("l" , l)
+    print("limit" , limit)
 
     response = await async_client.get(f"/api/users?last={l}&limit={limit}")
     
 
     assert response.status_code == 200
+
+    if len(response.json()) == 0:
+        assert response.json() == []
+        return
+
     assert list(response.json()[0].keys()) == [ "name","id","email","avatar"]
 
 @pytest.mark.parametrize("user",get_random_user_list())
@@ -95,26 +113,28 @@ async def test_get_user_by_id(async_client,user):
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("user",get_random_user_list())
+@pytest.mark.parametrize("user",get_random_user_list_for_update())
 async def test_update_user(async_client,user):
     user_id = await get_user_id(async_client,user)
     access_token = await get_access_token(async_client,user)
     
     payload = user.copy()
-    payload["name"] += " Updated" + str(random.randint(0,100))
     payload["avatar"] = "avatar_url_updated" + str(random.randint(0,100))
-    payload["age"] = random.randint(0,100)
+    payload["age"] = random.randint(1,99)
 
     response = await async_client.put(f"/api/users/{user_id}",json=payload,headers={"Authorization": f"Bearer {access_token}"})
     assert response.status_code == 200
-    assert response.json()["name"] == payload["name"]
     assert response.json()["avatar"] == payload["avatar"]
     assert response.json()["age"] == payload["age"]
+
+    # reset
+    await async_client.put(f"/api/users/{user_id}",json=user.copy(),headers={"Authorization": f"Bearer {access_token}"})
+    assert response.status_code == 200
 
 
 @pytest.mark.parametrize("user",get_random_user_list())
 @pytest.mark.asyncio
-async def test_get_user_by_id2(async_client,user):
+async def test_get_user_by_id_2(async_client,user):
     user_id = await get_user_id(async_client,user)
     response = await async_client.get(f"/api/users/{user_id}")
 
@@ -122,12 +142,14 @@ async def test_get_user_by_id2(async_client,user):
     assert list(response.json().keys()) == [ "name","id","birthday","age","avatar"]
 
 
-
 @pytest.mark.parametrize("l,r",get_random_page_range_list())
 @pytest.mark.asyncio
 async def test_get_users_2(async_client,l,r):
     limit = r - l
-    response = await async_client.get("/api/users?last={l}&limit={limit}")
+    print("l" , l)
+    print("limit" , limit)
+
+    response = await async_client.get(f"/api/users?last={l}&limit={limit}")
     
 
     assert response.status_code == 200
