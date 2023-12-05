5b8ce944e7897169145d27bf6025d6bed24addfe
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-12-06 00:45:33 +0800

    decoupling config files

--- backend/setting/config.py
+++ backend/setting/config.py
@@ -1,48 +1,45 @@
 import os
 from functools import lru_cache
 
-from dotenv import load_dotenv
-
-class Settings():
+class BaseSettings():
     app_name:str = "iThome2023 FastAPI Tutorial"
     author:str = "Jason Liu"
 
-    app_mode: str = os.getenv("APP_MODE")
+    app_mode: str = os.getenv("APP_MODE").upper()
     port:int = int(os.getenv("PORT"))
-    reload:bool = os.getenv("RELOAD")
-    
+    reload:bool = os.getenv("RELOAD") 
+
+    access_token_secret:str = os.getenv("ACCESS_TOKEN_SECRET")
+    access_token_expire_minutes:int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+    refresh_token_secret:str = os.getenv("REFRESH_TOKEN_SECRET")
+    refresh_token_expire_minutes:int = int(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES"))
+
+    sentry_dsn:str = os.getenv("SENTRY_DSN")
+
+class Settings(BaseSettings):
     db_type:str = os.getenv("DB_TYPE").upper()
-    run_mode:str = os.getenv("RUN_MODE").upper()
-    database_url: str = os.getenv(f"{run_mode}_{db_type}_DATABASE_URL")
+    database_url: str = os.getenv("DATABASE_URL")
+    redis_url:str = os.getenv("REDIS_URL")
+    
+
+class PrimaryReplicaSetting(BaseSettings):
+    primary_database_url: str = os.getenv("PRIMARY_DATABASE_URL")
+    replica_database_url: str = os.getenv("REPLICA_DATABASE_URL")
     redis_url:str = os.getenv("REDIS_URL") 
-
-    access_token_secret:str = os.getenv("ACCESS_TOKEN_SECRET")
-    access_token_expire_minutes:int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-
-    refresh_token_secret:str = os.getenv("REFRESH_TOKEN_SECRET")
-    refresh_token_expire_minutes:int = int(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES"))
-
-
-class PrimaryReplicaSetting():
-    primary_database_url: str = os.getenv("ASYNC_PRIMARY_DATABASE_URL")
-    replica_database_url: str = os.getenv("ASYNC_REPLICA_DATABASE_URL")
-    redis_url:str = os.getenv("REDIS_URL") 
-
-    access_token_secret:str = os.getenv("ACCESS_TOKEN_SECRET")
-    access_token_expire_minutes:int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-
-    refresh_token_secret:str = os.getenv("REFRESH_TOKEN_SECRET")
-    refresh_token_expire_minutes:int = int(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES"))
     
 
 
 @lru_cache()
 def get_settings():
-    load_dotenv( f".env.{os.getenv('APP_MODE')}")
-    return Settings()
 
+    if os.getenv("APP_MODE") is 'primary-replica':
+        return PrimaryReplicaSetting()
 
-@lru_cache()
-def get_primary_replica_settings():
-    load_dotenv( f".env.{os.getenv('APP_MODE')}")
-    return PrimaryReplicaSetting()
+    setting_cls_dict = {
+        "DEV":Settings,
+        "TEST":Settings,
+        "PROD":Settings,
+    }
+
+    return setting_cls_dict[os.getenv("APP_MODE").upper()]()
