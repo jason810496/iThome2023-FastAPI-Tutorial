24a198fe4bb13e6365334485a90f30e7b705b664
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-12-06 00:40:48 +0800

    add jekins setup script

---
+++ jekins/jenkins-setup.sh
@@ -0,0 +1,28 @@
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}Creating Jenkins Network${NC}"
+docker network create jenkins
+
+
+echo -e "${GREEN}Creating Jenkins Volumes${NC}"
+docker run --name jenkins-docker --rm --detach \
+  --privileged --network jenkins --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 \
+  docker:dind --storage-driver overlay2
+
+echo -e "${GREEN}Building Jenkins Image${NC}"
+docker build -t myjenkins-blueocean .
+
+
+echo -e "${GREEN}Creating Jenkins Container${NC}"
+docker run --name jenkins-blueocean --restart=on-failure --detach \
+  --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
+  --publish 8080:8080 --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  myjenkins-blueocean
