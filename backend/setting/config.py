fe6225e93d1b7179a6e5e2068208d74dd03808ee
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-12-12 01:35:31 +0800

    add mq setting

--- backend/setting/config.py
+++ backend/setting/config.py
@@ -2,39 +2,52 @@ import os
 from functools import lru_cache
 
 class BaseSettings():
-    app_name:str = "iThome2023 FastAPI Tutorial"
-    author:str = "Jason Liu"
+    def __init__(self) -> None:
+        self.app_name:str = "iThome2023 FastAPI Tutorial"
+        self.author:str = "Jason Liu"
 
-    app_mode: str = os.getenv("APP_MODE").upper()
-    port:int = int(os.getenv("PORT"))
-    reload:bool = os.getenv("RELOAD") 
+        self.app_mode: str = os.getenv("APP_MODE").upper()
+        self.port:int = int(os.getenv("PORT"))
+        self.reload:bool = os.getenv("RELOAD") 
 
-    access_token_secret:str = os.getenv("ACCESS_TOKEN_SECRET")
-    access_token_expire_minutes:int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+        self.access_token_secret:str = os.getenv("ACCESS_TOKEN_SECRET")
+        self.access_token_expire_minutes:int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
-    refresh_token_secret:str = os.getenv("REFRESH_TOKEN_SECRET")
-    refresh_token_expire_minutes:int = int(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES"))
+        self.refresh_token_secret:str = os.getenv("REFRESH_TOKEN_SECRET")
+        self.refresh_token_expire_minutes:int = int(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES"))
 
-    sentry_dsn:str = os.getenv("SENTRY_DSN")
+        self.sentry_dsn:str = os.getenv("SENTRY_DSN")
 
 class Settings(BaseSettings):
-    db_type:str = os.getenv("DB_TYPE").upper()
-    database_url: str = os.getenv("DATABASE_URL")
-    redis_url:str = os.getenv("REDIS_URL")
+    def __init__(self) -> None:
+        self.db_type:str = os.getenv("DB_TYPE").upper()
+        self.database_url: str = os.getenv("DATABASE_URL")
+        self.redis_url:str = os.getenv("REDIS_URL")
     
 
 class PrimaryReplicaSetting(BaseSettings):
-    primary_database_url: str = os.getenv("PRIMARY_DATABASE_URL")
-    replica_database_url: str = os.getenv("REPLICA_DATABASE_URL")
-    redis_url:str = os.getenv("REDIS_URL") 
+    def __init__(self) -> None:
+        self.primary_database_url: str = os.getenv("PRIMARY_DATABASE_URL")
+        self.replica_database_url: str = os.getenv("REPLICA_DATABASE_URL")
+        self.redis_url:str = os.getenv("REDIS_URL") 
+
+class MessageQueueSettings():
+    def __init__(self) -> None:
+        self.upload_path:str = os.getenv("UPLOAD_PATH")
+        self.tmp_path:str = os.getenv("TMP_PATH")
+        self.redis_url:str = os.getenv("REDIS_URL")
+        self.queue_name:str = os.getenv("QUEUE_NAME")
     
 
 
 @lru_cache()
 def get_settings():
 
-    if os.getenv("APP_MODE") is 'primary-replica':
+    if os.getenv("APP_MODE") == 'primary-replica':
         return PrimaryReplicaSetting()
+    
+    if os.getenv("APP_MODE") == 'MESSAGE-QUEUE':
+        return MessageQueueSettings()
 
     setting_cls_dict = {
         "DEV":Settings,
